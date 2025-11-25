93d4fd203663b1989271c985fadc83ff6ac9e539
hanna_@kapustka <hannanapieracz@gmail.com>
2025-11-25 23:42:18 +0100

    domek3.sh

---
+++ domek3.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+declare -a tablica=(100 12 9 70 16 0 81)
+
+tablica[7]=2137
+tablica[8]=420
+tablica[9]=67
+
+echo "${tablica[@]}"
+
+z=${#tablica[@]}
+
+for ((i=0; i<z-1; i++)); do
+    for ((j=0; j<z-1-i; j++)); do
+        if [[ ${tablica[j]} -gt ${tablica[j+1]} ]]; then
+            bufor=${tablica[j]}
+            tablica[j]=${tablica[j+1]}
+            tablica[j+1]=$bufor
+        fi
+    done
+done
+
+echo "posortowana tablica: ${tablica[@]}"
+echo "minimalna liczba: ${tablica[0]}"
